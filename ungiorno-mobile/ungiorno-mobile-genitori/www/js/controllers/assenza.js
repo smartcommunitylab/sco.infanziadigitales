a0bf5adf668bce7149c610e54a75689a4cd2c765
pietrocamin <pietro.camin@gmail.com>
2015-06-19 17:00:31 +0200

    modifiche assenze

--- ungiorno-mobile/ungiorno-mobile-genitori/www/js/controllers/assenza.js
+++ ungiorno-mobile/ungiorno-mobile-genitori/www/js/controllers/assenza.js
@@ -1,5 +1,17 @@
 angular.module('it.smartcommunitylab.infanziadigitales.diario.parents.controllers.assenza', [])
 
 .controller('AssenzaCtrl', function ($scope) {
-
+    $scope.isMalattia = false;
+    $scope.showSelect = function(intInput)
+    {
+        if(intInput === 1)
+        {
+            $scope.isMalattia = true;
+        }
+        else
+        {
+            $scope.isMalattia = false;
+        }
+    }
+    $scope.malattieArray = ['Influenza', 'Varicella', 'Pertosse'];
 });

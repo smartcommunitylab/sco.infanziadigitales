613807172e392b0b5de8341ba05accb2abe737b6
Matteo Chini <matteo.chini@gmail.com>
2015-06-22 17:54:31 +0200

    added new internationalization component for date, added day on home page navigation bar

--- ungiorno-mobile/ungiorno-mobile-genitori/www/js/controllers/home.js
+++ ungiorno-mobile/ungiorno-mobile-genitori/www/js/controllers/home.js
@@ -2,7 +2,24 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.parents.controller
 
 .controller('HomeCtrl', function ($scope, $location) {
 
+    $scope.date = "";
+
     $scope.goTo = function (location) {
         window.location.assign(location);
+    }
+
+    $scope.getDateString = function () {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1; //January is 0!
+
+        var yyyy = today.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd
+        }
+        if (mm < 10) {
+            mm = '0' + mm
+        }
+        $scope.date = today.getTime();;
     }
 });

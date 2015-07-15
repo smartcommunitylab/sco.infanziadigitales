1122dca0d512ca31e7dd518fd2fb46492c45e5de
Matteo Chini <matteo.chini@gmail.com>
2015-07-15 11:02:24 +0200

    fix retire preselection

--- ungiorno-mobile/ungiorno-mobile-genitori/www/js/controllers/retire.js
+++ ungiorno-mobile/ungiorno-mobile-genitori/www/js/controllers/retire.js
@@ -2,6 +2,7 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.parents.controller
 
 .controller('RetireCtrl', function ($scope, configurationService, profileService, dataServerService, Toast, $ionicHistory, retireService, $filter) {
     var retireConfiguration;
+    $scope.retirePersons = [];
 
     function getTime() {
         var dateInsert = new Date($scope.temporary.date);
@@ -18,6 +19,10 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.parents.controller
         }
     }
 
+    function setDefaultPErsonId() {
+        radio[1].checked = true;
+    }
+
     function getSelectedPersonId() {
         var radio = document.getElementsByName('radio');
         for (var i = 0; i < radio.length; i++) {
@@ -30,9 +35,27 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.parents.controller
 
     $scope.babyProfile = profileService.getBabyProfile();
     $scope.babyConfiguration = configurationService.getBabyConfiguration();
+    for (var k in $scope.babyProfile.persons) {
+        $scope.retirePersons.push({
+            fullName: $scope.babyProfile.persons[k].fullName,
+            authorizationDeadline: $scope.babyProfile.persons[k].authorizationDeadline,
+            checked: ($scope.babyConfiguration.defaultPerson == $scope.babyProfile.persons[k].personId)
+
+
+        })
+    }
+    if ($scope.babyConfiguration.extraPersons != null) {
+        $scope.retirePersons.push({
+            fullName: $scope.babyConfiguration.extraPersons.fullName,
+            authorizationDeadline: $scope.babyConfiguration.extraPersons.authorizationDeadline,
+            checked: ($scope.babyConfiguration.defaultPerson == $scope.babyConfiguration.extraPersons.personId)
+
+        });
+    }
+    //set default time (forget the days, because it is overwritten by new date
     $scope.temporary = {
         date: new Date(),
-        time: new Date(),
+        time: new Date($scope.babyConfiguration.exitTime),
         note: null
     };
     $scope.temporary.time.setHours($scope.temporary.time.getHours(), $scope.temporary.time.getMinutes(), 0, 0)

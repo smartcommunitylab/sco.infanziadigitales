0e2fd433050b1b987e1f9eb0d06ee76d9f752703
dema121 <andrea.dematte@gmail.com>
2015-08-05 17:17:27 +0200

    fix for status and now it gets data from profileService and babyConfigurationService

--- ungiorno-mobile/ungiorno-mobile-insegnante/www/js/controllers/babyprofileCtrl.js
+++ ungiorno-mobile/ungiorno-mobile-insegnante/www/js/controllers/babyprofileCtrl.js
@@ -1,18 +1,25 @@
 angular.module('it.smartcommunitylab.infanziadigitales.diario.teachers.controllers.babyprofile', [])
 
-.controller('babyprofileCtrl', function ($scope, $location, dataServerService, profileService) {
+.controller('babyprofileCtrl', function ($scope, $location, dataServerService, profileService, babyConfigurationService, $filter) {
 
     //Acquiring data from server
     $scope.babyProfile = profileService.getBabyProfile();
-    //$scope.babyConfig = profileService.getBabyConfig();
+    $scope.schoolProfile = profileService.getSchoolProfile();
+    $scope.babyConfig = babyConfigurationService.getBabyConfiguration();
     //temp babyConfig
-    dataServerService.getBabyConfiguration().then(function (data) {
-        $scope.babyConfig = data[0];
-        $scope.babyStatus = new Date().getTime() > $scope.babyConfig.exitTime ? "uscito" : "presente";
-    },
-    function (error) {
-        console.log("ERROR -> " + error);
-    });
+
+    $scope.babyEnterHour = $scope.babyConfig.services.anticipo ? $scope.schoolProfile.anticipoTiming.fromTime : $scope.schoolProfile.regularTiming.fromTime;
+    $scope.babyExitHour = $scope.babyConfig.services.posticipo ? $scope.schoolProfile.posticipoTiming.toTime : $scope.schoolProfile.regularTiming.toTime;
+
+    //used to get if the baby is present
+    var today = new Date();
+    var exitDayWithHour = new Date();
+    var exitHour = new Date('01/01/2000 ' + $scope.babyExitHour); //placeholder date, well completed after
+    exitDayWithHour.setHours(exitHour.getHours());
+    exitDayWithHour.setMinutes(exitHour.getMinutes());
+
+
+    $scope.babyStatus = today.getTime() > exitDayWithHour.getTime() ? $filter('translate')('exit') : $filter('translate')('present');
 
     dataServerService.getNotes().then(function (data) {
         $scope.notes = data[0];
@@ -32,12 +39,7 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.teachers.controlle
         return $scope.babyConfig.services.bus.active;
     }
     $scope.isDelegation = function() {
-        return true;
-    }
-
-
-    $scope.newNoteDialog = function() {
-
+        return $scope.babyConfig.extraPerson.personId === $scope.babyConfig.personWhoRetireBaby.personId;
     }
 
 });

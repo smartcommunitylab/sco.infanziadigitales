aec5b6512e1e600fa149e363a1dba4574136b4fe
Matteo Chini <matteo.chini@gmail.com>
2015-08-11 09:24:28 +0200

    Skeleton for students

--- ungiorno-mobile/ungiorno-mobile-insegnante/www/js/controllers/communicationsCtrl.js
+++ ungiorno-mobile/ungiorno-mobile-insegnante/www/js/controllers/communicationsCtrl.js
@@ -1,7 +1,9 @@
 angular.module('it.smartcommunitylab.infanziadigitales.diario.teachers.controllers.communications', [])
 
-.controller('communicationsCtrl', function ($scope, $location) {
+.controller('communicationsCtrl', function ($scope, $location, $ionicHistory) {
 
-
-
+    //    creare un oggetto che memorizza la lista di comunicazioni a livello di scope
+    //    chiamare la funzione che scarica le comunicazioni dal server e associarle alla lista creata
+    //    creare una funzione che gestisca la memorizzazione della comunicazione selezionata
+    //    (memorizzo tutta la comunicazione? o solo l'indice dell'array?)
 });

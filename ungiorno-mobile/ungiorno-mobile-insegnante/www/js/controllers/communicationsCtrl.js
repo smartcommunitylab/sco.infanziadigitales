fed87a2ff4db483a0986168a9e1b15a58a61eb11
danielx007 <gotca@fbk.eu>
2015-08-18 09:44:33 +0200

    communication page

--- ungiorno-mobile/ungiorno-mobile-insegnante/www/js/controllers/communicationsCtrl.js
+++ ungiorno-mobile/ungiorno-mobile-insegnante/www/js/controllers/communicationsCtrl.js
@@ -1,6 +1,6 @@
 angular.module('it.smartcommunitylab.infanziadigitales.diario.teachers.controllers.communications', [])
 
-.controller('communicationsCtrl', function ($scope, $location, $ionicHistory, dataServerService) {
+.controller('communicationsCtrl', function ($scope, $location, $ionicHistory, dataServerService, $ionicPopup, $timeout) {
 
     var selectedCommunicationIndex = -1;
     var selectedNewCommunication = false;
@@ -10,6 +10,8 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.teachers.controlle
     var editClose = false;
     var selectedComIndex = null;
     var selectedComIndexCopy = null;
+    var editShowButton = true;
+    $scope.docCheck = false;
     $scope.communicationTypes = [
         { typeId : "Generica",
           checked : false},
@@ -17,13 +19,25 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.teachers.controlle
         { typeId : "Consegna Documenti",
           checked : false}
     ];
+    $scope.newCommunication = {
+            appId : "a",
+            schoolId : "a",
+            dateToCheck : 0,
+            creationDate : 0,
+            description : "",
+            doCheck : false,
+            children : []
+     },
 
-     dataServerService.getCommunications().then(function(data){
+        dataServerService.getCommunications().then(function(data){
         $scope.communications = data;
+            for (var i = 0; i < $scope.communications.length; i++){
+                $scope.communications[i].dateToCheck *= 1000;
+            }
      });
 
     $scope.selectCommunication = function(index){
-        if (!modifyState) {
+        if (!modifyState && !selectedNewCommunication) {
 
             if (selectedCommunicationIndex === index) {
                 selectedCommunicationIndex=-1;
@@ -47,6 +61,7 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.teachers.controlle
     $scope.modifyDescription = function (index) {
 
         if (selectedComIndex == index && modifyState){
+            editShowButton = false;
             return true;
         } else {
         return false;}
@@ -56,7 +71,7 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.teachers.controlle
     }
 
     $scope.controlDateToCheck = function(index){
-        return $scope.communications[index].dateToCheck !== null;
+        return $scope.communications[index].doCheck ;
     }
 
     $scope.isCommunicationSelectedMode = function() {
@@ -81,7 +96,7 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.teachers.controlle
         return false;}
     }
      $scope.deleteNewCommunication = function() {
-      return deleteCommunication === false;
+      return deleteCommunication ;
     }
 
 
@@ -96,30 +111,45 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.teachers.controlle
         }
 
     $scope.buttonShow = function (communication) {
+        if (!modifyState){
         return communication !== null;
+        }
     }
 
     $scope.editCom = function (){
 
       $scope.communications[selectedComIndex].dateToCheck /= 1000;
-
       modifyState = false;
       selectedCommunicationIndex = -1;
-
+      editShowButton = true;
     }
 
+    $scope.editShowButton = function () {
+        return editShowButton;
+    }
 
-      /*function sendCom() {
-        for (var i = 0; i < $scope.retirePersons.length; i++) {
-            if ($scope.communicationType[i].checked == true) {
-                return $scope.communicationType[i].typeId;
+    $scope.deleteCommunication = function() {
+        $scope.communications.splice(selectedCommunicationIndex, 1);
+         selectedCommunicationIndex = -1;
+     }
+
+    $scope.sendCom = function() {
+            $scope.newCommunication.dateToCheck = document.getElementById("deadlineDate").value;
+            $scope.newCommunication.description = document.getElementById("description").value;
+            $scope.newCommunication.creationDate = new Date();
+            $scope.newCommunication.doCheck = $scope.docCheck;
+            $ionicPopup.alert({
+                 template: 'Comunicazione inviata'
+               });
+        }
+    $scope.check = function (){
+        if ($scope.docCheck === true){
+              $scope.docCheck = false;
+            } else {
+              $scope.docCheck = true;
             }
-            var noteTypeID = document.getElementById("deadlineDate").value;
-            var noteTypeID = document.getElementById("note_type").value
-        } id="modifyDateToCheck"
-        return null;
+    }
 
-    }*/
     //    creare un oggetto che memorizza la lista di comunicazioni a livello di scope
     //    chiamare la funzione che scarica le comunicazioni dal server e associarle alla lista creata
     //    creare una funzione che gestisca la memorizzazione della comunicazione selezionata

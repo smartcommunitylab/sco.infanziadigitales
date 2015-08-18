cab20ffc03d442b0b588acc3e7ddcfe33c9ee28e
dema121 <andrea.dematte@gmail.com>
2015-08-18 17:07:11 +0200

    implemented take photo, select from gallery for new post. Fix for plus button (new post) style

--- ungiorno-mobile/ungiorno-mobile-diario-condiviso/www/js/controllers/homeCtrl.js
+++ ungiorno-mobile/ungiorno-mobile-diario-condiviso/www/js/controllers/homeCtrl.js
@@ -1,10 +1,10 @@
 angular.module('it.smartcommunitylab.infanziadigitales.diario.diariocondiviso.controllers.home', [])
 
-.controller('HomeCtrl', function ($scope, dataServerService, $ionicModal) {
+.controller('HomeCtrl', function ($scope, dataServerService, $ionicModal, $cordovaCamera, $ionicPopover) {
 
     var newPostModal;
+    var photoSrcSelect;
     $scope.today = new Date();
-    var postToCreate;
 
     $scope.attachedTags = [];
 
@@ -29,29 +29,83 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.diariocondiviso.co
     });
 
 	$scope.createPost = function() {
-        postToCreate = new Object();
+        $scope.postToCreate = new Object();
+        $scope.postToCreate.photos = [];
         $scope.setMood(0);
         $scope.newPost.date = new Date();
         $scope.newPost.description = "";
-        //$scope.newPost.photos = "";
+        $scope.newPost.photos = [];
         $scope.attachedTags = [];
 		newPostModal.show();
 	}
 
 	$scope.newPost = function(post) {
-        postToCreate.description = $scope.newPost.description;
+        $scope.postToCreate.description = $scope.newPost.description;
         $scope.attachedTags.forEach(function (obj) { //hashkey generated for some random reason, remove it!
             delete obj["$$hashKey"];
         });
-        postToCreate.tags = $scope.attachedTags;
-        //postToCreate.photos = $scope.newPost.photos;
-        postToCreate.date = $scope.newPost.date;
-        console.log(JSON.stringify(postToCreate));
+        $scope.postToCreate.tags = $scope.attachedTags;
+        $scope.postToCreate.date = $scope.newPost.date;
+        console.log(JSON.stringify($scope.postToCreate));
         newPostModal.hide();
 	}
 
     $scope.setMood = function (moodCode) {
-       postToCreate.mood = moodCode;
+       $scope.postToCreate.mood = moodCode;
+    }
+
+    $scope.addPhoto = function ($event, photoSrc) {
+
+        if (photoSrc === undefined) {
+
+            var template = '<ion-popover-view><ion-content><ion-list><ion-item ng-click="addPhoto($event, \'Camera\')">Camera</ion-item><ion-item ng-click="addPhoto($event, \'Gallery\')">Gallery</ion-item></ion-list></ion-content></ion-popover-view>';
+
+            if (photoSrcSelect === undefined) {
+                photoSrcSelect = $ionicPopover.fromTemplate(template, {
+                    scope: $scope
+                });
+            }
+
+            photoSrcSelect.show($event);
+        } else {
+            if (photoSrcSelect !== undefined) {
+                photoSrcSelect.hide();
+            }
+            var options = {};
+
+            if (photoSrc === 'Camera') {
+                options = {
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    sourceType: Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
+                    allowEdit: false,
+                    encodingType: Camera.EncodingType.JPEG,
+                    popoverOptions: CameraPopoverOptions,
+                    saveToPhotoAlbum: false
+                };
+            } else {
+                options = {
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    sourceType: Camera.PictureSourceType.PHOTOLIBRARY, // Camera.PictureSourceType.PHOTOLIBRARY
+                    allowEdit: false,
+                    encodingType: Camera.EncodingType.JPEG,
+                    popoverOptions: CameraPopoverOptions,
+                    saveToPhotoAlbum: false
+                };
+            }
+
+            $cordovaCamera.getPicture(options).then(function (imageData) {
+                var image;
+                image = "data:image/jpeg;base64," + imageData;
+                $scope.postToCreate.photos.push(image);
+                $scope.imageBase64.push(imageData);
+            }, function (err) {
+                console.log(err);
+            });
+        }
+    }
+
+    $scope.removePhoto = function (index) {
+        $scope.postToCreate.photos.splice(index, 1);
     }
 
 });

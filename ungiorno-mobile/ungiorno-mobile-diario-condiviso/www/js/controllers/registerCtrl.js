19e7797b08b5f5f8d7b1fbaa75654802437ee6bb
danieltimis <timis@fbk.eu>
2015-08-20 13:39:11 +0200

    edit register

--- ungiorno-mobile/ungiorno-mobile-diario-condiviso/www/js/controllers/registerCtrl.js
+++ ungiorno-mobile/ungiorno-mobile-diario-condiviso/www/js/controllers/registerCtrl.js
@@ -2,6 +2,24 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.diariocondiviso.co
 
 .controller('RegisterCtrl', function ($scope, dataServerService) {
 
+    $scope.enterEmail = false;
+
+
+    $scope.openEnterEmail = function () {
+        return $scope.enterEmail;
+
+    }
+
+    $scope.mailFb = function () {
+        $scope.enterEmail = true;
+
+    }
+
+    $scope.mailGm = function () {
+        $scope.enterEmail = true;
+
+    }
+
         $scope.steps = [
         {
             done: true
@@ -16,5 +34,7 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.diariocondiviso.co
             done: false
         },
         ];
+
+
 
 });

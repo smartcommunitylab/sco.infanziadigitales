914e78e27c98d00965c84277bb16876135a99aa0
S4mu3l3P <samuelepinginf@gmail.com>
2016-05-16 08:25:40 +0200

    login

--- ungiorno-mobile/ungiorno-mobile-diario-condiviso/www/js/controllers/loginCtrl.js
+++ ungiorno-mobile/ungiorno-mobile-diario-condiviso/www/js/controllers/loginCtrl.js
@@ -16,6 +16,11 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.diariocondiviso.co
         password: ''
     };*/
 
+    var loginStarted = false;
+    $scope.user = {
+        email: '',
+        password: ''
+    };
     $scope.login = function (provider) {
         loginService.login(provider).then(
             function (data) {
@@ -76,11 +81,70 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.diariocondiviso.co
             },
             function (error) {
                 localStorage.userId = null;
+                $scope.user.password= '';
                 $ionicPopup.alert({
                     title: $filter('translate')('error_popup_title'),
                     template: $filter('translate')('error_signin')
                 });
 
+            }
+        ).finally($ionicLoading.hide());
+    };
+})
+
+
+.controller('RegisterCtrl', function ($scope, $rootScope, $state, $filter, $ionicHistory, $ionicPopup, loginService, Config, $translate, $ionicLoading) {
+    $scope.user = {
+        lang: $translate.preferredLanguage(),
+        name: '',
+        surname: '',
+        email: '',
+        password: ''
+    };
+
+    var validate = function () {
+        if (!$scope.user.name.trim() || !$scope.user.surname.trim() || !$scope.user.email.trim() || !$scope.user.password.trim()) {
+            return 'error_required_fields';
+        }
+        if ($scope.user.password.trim().length < 6) {
+            return 'error_password_short';
+        }
+        return null;
+    };
+
+    $scope.toLogin = function () {
+        window.location.reload(true);
+    }
+
+    $scope.resend = function () {
+        window.open(Config.getAACURL() + '/internal/resend?lang=en', '_system', 'location=no,toolbar=no')
+    }
+
+
+    $scope.register = function () {
+        var msg = validate();
+        if (msg) {
+            $ionicPopup.alert({
+                title: $filter('translate')('error_popup_title'),
+                template: $filter('translate')(msg)
+            });
+            return;
+        }
+
+        $ionicLoading.show();
+        loginService.register($scope.user).then(
+            function (data) {
+                $state.go('app.signupsuccess');
+            },
+            function (error) {
+                var errorMsg = 'error_generic';
+                if (error == 409) {
+                    errorMsg = 'error_email_inuse';
+                }
+                $ionicPopup.alert({
+                    title: $filter('translate')('error_popup_title'),
+                    template: $filter('translate')(errorMsg)
+                });
             }
         ).finally($ionicLoading.hide());
     };

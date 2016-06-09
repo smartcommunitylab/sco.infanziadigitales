2324ebc077168b43c57eb1ffd39d686547eba853
chin8 <matteo.chini@fbk.eu>
2016-06-09 10:46:02 +0200

    list of kids in alphabetically order

--- ungiorno-mobile/ungiorno-mobile-diario-condiviso/www/js/filter.js
+++ ungiorno-mobile/ungiorno-mobile-diario-condiviso/www/js/filter.js
@@ -99,6 +99,24 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.diariocondiviso.fi
     };
 })
 
+.filter('orderObjectBy', function () {
+    return function (input, attribute) {
+        if (!angular.isObject(input)) return input;
+
+        var array = [];
+        for (var objectKey in input) {
+            array.push(input[objectKey]);
+        }
+
+        array.sort(function (a, b) {
+            var alc = a[attribute].toLowerCase(),
+                blc = b[attribute].toLowerCase();
+            return alc > blc ? 1 : alc < blc ? -1 : 0;
+        });
+        return array;
+    }
+})
+
 .filter('capitalize', function () {
     return function (input, scope) {
         if (input != null) {

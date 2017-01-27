3a5ec3c1d7afea3b0547f9e76017d0f824ccfcc4
Admin <Admin@MacBook-Pro-2.local>
2017-01-27 10:54:19 +0100

    new version code

--- ungiorno-mobile/ungiorno-mobile-genitori/www/js/services/conf.js
+++ ungiorno-mobile/ungiorno-mobile-genitori/www/js/services/conf.js
@@ -16,7 +16,7 @@ angular.module('it.smartcommunitylab.infanziadigitales.diario.parents.services.c
 
   var APP_BUILD = '';
 
-  var APP_VERSION = '2.0.4';
+  var APP_VERSION = '2.0.5';
   var mapJsonConfig = null;
 
 

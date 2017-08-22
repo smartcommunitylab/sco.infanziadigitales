2c40190046bd50d4033dcb14661f2e73ec7090aa
mirkoperillo <mirko.perillo@gmail.com>
2017-08-22 13:21:30 +0200

    clean commented code

--- ungiorno-console/src/app/in-memory-data.service.ts
+++ ungiorno-console/src/app/in-memory-data.service.ts
@@ -10,47 +10,6 @@ import { Group } from "./Classes/group";
 import { Parent } from "./Classes/parent";
 
 export class InMemoryDataService implements InMemoryDbService {
-  // createDb() {
-  //   var school = new School();
-  //   school.id = "TN2000G"
-  //   school.name = "Scuola Uno"
-  //   school.address = "Via Dante 1, Citta, Provincia"
-  //   school.telephone = "0461256985"
-  //   school.email = "scuolauno@mail.com"
-  //   school.kids = [
-  //     new Kid("RSSMRO", "Mario", "Rossi"),
-  //     new Kid("VRDGVN","Giovanni", "Verdi"),
-  //     new Kid("BNCLGI","Luigi", "Bianchi"),
-  //   ];
-  //   school.teachers = [
-  //     new Teacher("FSTANT", "Antonello", "Fausti"),
-  //     new Teacher("FSTLGI","Luigi", "Festi"),
-  //     new Teacher("FRRMNL","Emanuela", "Ferri"),
-  //   ]
-  //   school.buses = [
-  //     new Bus("bus1", "Povo"),
-  //     new Bus("bus2", "Gardolo"),
-  //     new Bus("bus3", "Rovereto")
-  //   ];
-  //   school.servizi = [
-  //     new Service("default", [new Time("mattutina", new Date(0, 0, 0, 9), new Date(0, 0, 0, 13)), new Time("pomeridiana", new Date(0, 0, 0, 13), new Date(0, 0, 0, 16))]), //9.00 to 13.00
-  //     new Service("anticipo", [new Time("Fascia unica", new Date(0, 0, 0, 8, 30), new Date(0, 0, 0, 9, 30))]), //9.00 to 13.00
-  //     new Service("posticipo", [new Time("Fascia unica", new Date(0, 0, 0, 15, 30), new Date(0, 0, 0, 17, 30))]), //9.00 to 13.00
-  //   ]
-  //   school.sections = [
-  //     "Sezione1",
-  //     "Sezione2",
-  //     "Sezione3",
-  //     "Sezione4",
-  //   ];
-  //   school.assenze = [
-  //     "Malattia",
-  //     "Vaccinazione",
-  //     "Vacanza"
-  //   ];
-  //   return {school};
-  // }
-
     createDb() {
       const school = [
         {

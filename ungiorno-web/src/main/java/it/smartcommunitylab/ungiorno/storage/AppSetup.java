07161d346b06ce88a8caf684ade3dd5d02dc02bc
mirkoperillo <mirko.perillo@gmail.com>
2017-08-28 10:22:07 +0200

    store schoolprofiles bound to app setup if they doesn't exist

--- ungiorno-web/src/main/java/it/smartcommunitylab/ungiorno/storage/AppSetup.java
+++ ungiorno-web/src/main/java/it/smartcommunitylab/ungiorno/storage/AppSetup.java
@@ -1,7 +1,5 @@
 package it.smartcommunitylab.ungiorno.storage;
 
-import it.smartcommunitylab.ungiorno.model.AppInfo;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -16,69 +14,84 @@ import org.springframework.stereotype.Component;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
+import it.smartcommunitylab.ungiorno.model.AppInfo;
+import it.smartcommunitylab.ungiorno.model.School;
+import it.smartcommunitylab.ungiorno.model.SchoolProfile;
+
 @Component
 public class AppSetup {
 
-	@Value("classpath:/apps-info.yml")
-	private Resource resource;
+    @Value("classpath:/apps-info.yml")
+    private Resource resource;
 
-	@Autowired
-	private RepositoryManager storage;
-
-	@PostConstruct
-	public void init() throws IOException {
-		Yaml yaml = new Yaml(new Constructor(AppSetup.class));
-		AppSetup data = (AppSetup) yaml.load(resource.getInputStream());
-		this.apps = data.apps;
-		this.uploadDirectory = data.uploadDirectory;
-
-		for (AppInfo cred: data.getApps()) {
-			storage.createApp(cred);
-		}
-		
-		if (appsMap == null) {
-			appsMap = new HashMap<String, AppInfo>();
-			for (AppInfo app : apps) {
-				appsMap.put(app.getAppId(), app);
-			}
-		}		
-	}
+    @Autowired
+    private RepositoryManager storage;
 
 
-	private String uploadDirectory;
-	private List<AppInfo> apps;
-	private Map<String,AppInfo> appsMap;
+    @PostConstruct
+    public void init() throws IOException {
+        Yaml yaml = new Yaml(new Constructor(AppSetup.class));
+        AppSetup data = (AppSetup) yaml.load(resource.getInputStream());
+        this.apps = data.apps;
+        this.uploadDirectory = data.uploadDirectory;
 
-	public String getUploadDirectory() {
-		return uploadDirectory;
-	}
+        for (AppInfo cred : data.getApps()) {
+            storage.createApp(cred);
 
-	public void setUploadDirectory(String uploadDirectory) {
-		this.uploadDirectory = uploadDirectory;
-	}
+            // create schoolProfile bound to appId
+            for (School school : cred.getSchools()) {
+                SchoolProfile schoolProfile = new SchoolProfile();
+                schoolProfile.setSchoolId(school.getSchoolId());
+                schoolProfile.setAppId(cred.getAppId());
+                if (storage.getSchoolProfile(cred.getAppId(), school.getSchoolId()) == null) {
+                    storage.storeSchoolProfile(schoolProfile);
+                }
+            }
+        }
 
-	public List<AppInfo> getApps() {
-		return apps;
-	}
+        if (appsMap == null) {
+            appsMap = new HashMap<String, AppInfo>();
+            for (AppInfo app : apps) {
+                appsMap.put(app.getAppId(), app);
+            }
+        }
+    }
 
-	public void setApps(List<AppInfo> apps) {
-		this.apps = apps;
-	}
 
-	public Map<String, AppInfo> getAppsMap() {
-		return appsMap;
-	}
+    private String uploadDirectory;
+    private List<AppInfo> apps;
+    private Map<String, AppInfo> appsMap;
 
-	public void setAppsMap(Map<String, AppInfo> appsMap) {
-		this.appsMap = appsMap;
-	}
+    public String getUploadDirectory() {
+        return uploadDirectory;
+    }
 
-	@Override
-	public String toString() {
-		return "AppSetup [apps=" + apps + "]";
-	}
+    public void setUploadDirectory(String uploadDirectory) {
+        this.uploadDirectory = uploadDirectory;
+    }
 
-	public AppInfo findAppById(String username) {
-		return appsMap.get(username);
-	}
+    public List<AppInfo> getApps() {
+        return apps;
+    }
+
+    public void setApps(List<AppInfo> apps) {
+        this.apps = apps;
+    }
+
+    public Map<String, AppInfo> getAppsMap() {
+        return appsMap;
+    }
+
+    public void setAppsMap(Map<String, AppInfo> appsMap) {
+        this.appsMap = appsMap;
+    }
+
+    @Override
+    public String toString() {
+        return "AppSetup [apps=" + apps + "]";
+    }
+
+    public AppInfo findAppById(String username) {
+        return appsMap.get(username);
+    }
 }

4382069fbd6cb2acf25cfe6fb7e70a5250b8aa03
AlbanaCelepija <acelepija@fbk.eu>
2017-09-13 11:22:46 +0200

    migration(springBoot)pom config and structure

---
+++ ungiorno-web/src/main/java/it/smartcommunitylab/ungiorno/WebApplication.java
@@ -0,0 +1,15 @@
+package it.smartcommunitylab.ungiorno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}

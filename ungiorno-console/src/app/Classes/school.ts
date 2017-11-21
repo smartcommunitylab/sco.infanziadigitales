80376b923970be8b3b5ab72b59bc0c4f6d25d59a
chin8 <matteo.chini@fbk.eu>
2017-11-21 15:22:40 +0100

    fixed bug with empty array for school

--- ungiorno-console/src/app/Classes/school.ts
+++ ungiorno-console/src/app/Classes/school.ts
@@ -6,7 +6,7 @@ import { Group } from "./group";
 
 export class School {
     id: string;
-    appId:string;
+    appId: string;
     name: string;
     phoneNumbers: string[];
     email: string;
@@ -15,12 +15,12 @@ export class School {
     kids: Kid[];
     teachers: Teacher[];
     buses: Bus[];
-    assenze:string[];
+    assenze: string[];
     malattie: string[];
     groups: Group[];
-    fermate : string[];
-    malattia : boolean;
-    familiari : boolean;
+    fermate: string[];
+    malattia: boolean;
+    familiari: boolean;
     accessEmail: string;
 
     constructor() {
@@ -35,32 +35,41 @@ export class School {
         this.fermate = [];
     }
 
-    static copy(source:School): School {
+    static copy(source: School): School {
         let school = new School();
         school.copyInto(source);
         return school;
-    } 
+    }
 
-    copyInto(source:any) {
+    copyInto(source: any) {
         Object.assign(this, source)
         this.phoneNumbers = new Array();
-        source.phoneNumbers.forEach(x => this.phoneNumbers.push(x));
+        if (source.phoneNumbers)
+            source.phoneNumbers.forEach(x => this.phoneNumbers.push(x));
         this.servizi = new Array();
-        source.servizi.forEach(x => this.servizi.push(x));
+        if (source.servizi)
+            source.servizi.forEach(x => this.servizi.push(x));
         this.kids = new Array();
-        source.kids.forEach(x => this.kids.push(x));
+        if (source.kids)
+            source.kids.forEach(x => this.kids.push(x));
         this.teachers = new Array();
-        source.teachers.forEach(x => this.teachers.push(x));
+        if (source.teacher)
+            source.teachers.forEach(x => this.teachers.push(x));
         this.buses = new Array();
-        source.buses.forEach(x => this.buses.push(x));
+        if (source.buses)
+            source.buses.forEach(x => this.buses.push(x));
         this.assenze = new Array();
-        source.assenze.forEach(x => this.assenze.push(x));
+        if (source.assenze)
+            source.assenze.forEach(x => this.assenze.push(x));
         this.malattie = new Array();
-        source.malattie.forEach(x => this.malattie.push(x));
+        if (source.malattie)
+            source.malattie.forEach(x => this.malattie.push(x));
         this.groups = new Array();
-        source.groups.forEach(x => this.groups.push(x));
+        if (source.groups)
+            source.groups.forEach(x => this.groups.push(x));
         this.fermate = new Array();
-        source.fermate.forEach(x => this.fermate.push(x));
+        if (source.fermate)
+            source.fermate.forEach(x => this.fermate.push(x));
 
     }
 }

9926c2b5552c0bac8fa97588a75fa6206577e267
chin8 <matteo.chini@fbk.eu>
2017-11-29 15:49:51 +0100

    fixed mname of the groups and sections in baby profile

--- ungiorno-mobile/ungiorno-mobile-insegnante/www/js/app.js
+++ ungiorno-mobile/ungiorno-mobile-insegnante/www/js/app.js
@@ -520,9 +520,10 @@ return {
     all:'Tutti',
     com_dt:'Imposta scadenza comunicazione',
     com_del:'La comunicazione verrà eliminata nel giorno indicato',
-    auth_person:"Incaricato del ritiro",
+    auth_person:"L'incaricato del ritiro è ",
     none_f:"Nessuna fermata specificata",
-    no_person:"Nessuna persona specificata"
+    no_person:"Nessuna persona specificata",
+    baby_section:"Fa parte dei gruppi/sezioni:"
   });
 
   $translateProvider.preferredLanguage("it");
